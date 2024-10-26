09c649cd841867423a5c4dfb82fed16bbb113c59
Adekabang <iban.ninja@gmail.com>
2024-10-26 15:55:16 -0400

    change og

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'https://8labs.id/opengraph.jpg',
       navbar: {
         title: 'Cloud Environment',
         logo: {
